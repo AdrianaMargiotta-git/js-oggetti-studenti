29a269df457f29e09e851a4cb6c7b04aa0861369
AdrianaMargiotta <adrianamargiotta@protonmail.com>
2020-11-09 16:16:55 +0100

    Esercizio 2 ok

--- js/script.js
+++ js/script.js
@@ -16,6 +16,30 @@ $(document).ready(function(){
         //$('.stampa').append(key + ': ' + studente[key]); //stampa a schermo solo i valori
     };
 
-    
+    //Esercizio 2: Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+        //definiamo un array di studenti
+    var arrayStudenti = [
+        {
+            'nome' : 'Adriana',
+            'cognome' : 'Margiotta',
+            'eta' : 22,
+        },
+        {
+            'nome' : 'John',
+            'cognome' : 'Doe',
+            'eta' : 44,
+        },
+        {
+            'nome' : 'Friedrich',
+            'cognome' : 'Nietzsche',
+            'eta' : 176,
+        },
+    ];
+        //tramite il ciclo for stampo per ogni studente il nome e il cognome
+    for (var i = 0; i < arrayStudenti.length; i++) {
+        console.log(arrayStudenti[i].nome + ' ' + arrayStudenti[i].cognome); //li mostro nella console
+        //document.getElementById("stampa2").innerHTML = arrayStudenti[i].nome + ' ' + arrayStudenti[i].cognome;
+        //document.getElementById("stampa2").innerHTML = JSON.stringify(arrayStudenti[1].nome, null, 4);
+    }
 
 });
